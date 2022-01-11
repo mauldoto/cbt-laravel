63094bc092f46acb777e3e206e96834f7ad8ccf0
mauldoto <maulanaly97@gmail.com>
2022-01-11 16:00:06 +0700

    delete unused css and js

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,16 +10,17 @@
   <link rel="stylesheet" href="{{asset('assets/vendors/feather/feather.css')}}">
   <link rel="stylesheet" href="{{asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
   <link rel="stylesheet" href="{{asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/typicons/typicons.css')}}">
+  {{-- <link rel="stylesheet" href="{{asset('assets/vendors/typicons/typicons.css')}}"> --}}
   <link rel="stylesheet" href="{{asset('assets/vendors/simple-line-icons/css/simple-line-icons.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
+  {{-- <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}"> --}}
   <!-- endinject -->
   <!-- Plugin css for this page -->
-  {{-- <link rel="stylesheet" href="{{asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}"> --}}
   <link rel="stylesheet" href="{{asset('assets/js/select.dataTables.min.css')}}">
+  @stack('additional-plugin-css')
   <!-- End plugin css for this page -->
   <!-- inject:css -->
   <link rel="stylesheet" href="{{asset('assets/vendors/css/style.css')}}">
+  @stack('additional-css')
   <!-- endinject -->
   <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" />
 </head>
@@ -44,7 +45,7 @@
         <!-- partial:partials/_footer.html -->
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">CBT Team Sekolah Menenengah Kejuruan</span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
           </div>
         </footer>
@@ -60,22 +61,13 @@
   <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
-  <script src="{{asset('assets/vendors/chart.js/Chart.min.js')}}"></script>
-  <script src="{{asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
-  <script src="{{asset('assets/vendors/progressbar.js/progressbar.min.js')}}"></script>
-
+  @stack('additional-plugin-js')
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="{{asset('assets/js/off-canvas.js')}}"></script>
-  <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
   <script src="{{asset('assets/js/template.js')}}"></script>
-  <script src="{{asset('assets/js/settings.js')}}"></script>
-  <script src="{{asset('assets/js/todolist.js')}}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="{{asset('assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
-  <script src="{{asset('assets/js/dashboard.js')}}"></script>
-  <script src="{{asset('assets/js/Chart.roundedBarCharts.js')}}"></script>
+  @stack('additional-custom-js')
   <!-- End custom js for this page-->
 </body>
 
