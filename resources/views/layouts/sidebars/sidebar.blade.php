1126fa2873f02be0ad81b91769b06d10f9c14309
mauldoto <maulanaly97@gmail.com>
2022-01-12 23:46:27 +0700

    modify sidebar

--- resources/views/layouts/sidebars/sidebar.blade.php
+++ resources/views/layouts/sidebars/sidebar.blade.php
@@ -6,88 +6,77 @@
         <span class="menu-title">Dashboard</span>
       </a>
     </li>
-    <li class="nav-item nav-category">UI Elements</li>
+    <li class="nav-item nav-category">Menu</li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+      <a class="nav-link" data-bs-toggle="collapse" href="#question-pack-menu" aria-expanded="false" aria-controls="question-pack-menu">
         <i class="menu-icon mdi mdi-floor-plan"></i>
-        <span class="menu-title">UI Elements</span>
+        <span class="menu-title">Bank Soal</span>
         <i class="menu-arrow"></i> 
       </a>
-      <div class="collapse" id="ui-basic">
+      <div class="collapse" id="question-pack-menu">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">List</a></li>
         </ul>
       </div>
     </li>
-    <li class="nav-item nav-category">Forms and Datas</li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-        <i class="menu-icon mdi mdi-card-text-outline"></i>
-        <span class="menu-title">Form elements</span>
-        <i class="menu-arrow"></i>
+      <a class="nav-link" data-bs-toggle="collapse" href="#teacher-menu" aria-expanded="false" aria-controls="teacher-menu">
+        <i class="menu-icon mdi mdi-floor-plan"></i>
+        <span class="menu-title">Guru</span>
+        <i class="menu-arrow"></i> 
       </a>
-      <div class="collapse" id="form-elements">
+      <div class="collapse" id="teacher-menu">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">List</a></li>
         </ul>
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-        <i class="menu-icon mdi mdi-chart-line"></i>
-        <span class="menu-title">Charts</span>
-        <i class="menu-arrow"></i>
+      <a class="nav-link" data-bs-toggle="collapse" href="#student-menu" aria-expanded="false" aria-controls="student-menu">
+        <i class="menu-icon mdi mdi-floor-plan"></i>
+        <span class="menu-title">Siswa</span>
+        <i class="menu-arrow"></i> 
       </a>
-      <div class="collapse" id="charts">
+      <div class="collapse" id="student-menu">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">List</a></li>
         </ul>
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-        <i class="menu-icon mdi mdi-table"></i>
-        <span class="menu-title">Tables</span>
-        <i class="menu-arrow"></i>
+      <a class="nav-link" data-bs-toggle="collapse" href="#grade-menu" aria-expanded="false" aria-controls="grade-menu">
+        <i class="menu-icon mdi mdi-floor-plan"></i>
+        <span class="menu-title">Kelas</span>
+        <i class="menu-arrow"></i> 
       </a>
-      <div class="collapse" id="tables">
+      <div class="collapse" id="grade-menu">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">List</a></li>
         </ul>
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-        <i class="menu-icon mdi mdi-layers-outline"></i>
-        <span class="menu-title">Icons</span>
-        <i class="menu-arrow"></i>
+      <a class="nav-link" data-bs-toggle="collapse" href="#subject-menu" aria-expanded="false" aria-controls="subject-menu">
+        <i class="menu-icon mdi mdi-floor-plan"></i>
+        <span class="menu-title">Mata Pelajaran</span>
+        <i class="menu-arrow"></i> 
       </a>
-      <div class="collapse" id="icons">
+      <div class="collapse" id="subject-menu">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">List</a></li>
         </ul>
       </div>
     </li>
-    <li class="nav-item nav-category">pages</li>
+    <li class="nav-item nav-category">Media</li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-        <i class="menu-icon mdi mdi-account-circle-outline"></i>
-        <span class="menu-title">User Pages</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="auth">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item nav-category">help</li>
-    <li class="nav-item">
-      <a class="nav-link" href="http://bootstrapdash.com/demo/star-admin2-free/docs/documentation.html">
-        <i class="menu-icon mdi mdi-file-document"></i>
-        <span class="menu-title">Documentation</span>
+      <a class="nav-link" href="{{ url('/') }}">
+        <i class="mdi mdi-grid-large menu-icon"></i>
+        <span class="menu-title">Dashboard</span>
       </a>
     </li>
   </ul>
