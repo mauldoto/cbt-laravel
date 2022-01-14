d5af833af226ef1959c420057ae296e1142ac2e5
mauldoto <maulanaly97@gmail.com>
2022-01-15 03:17:41 +0700

    add helper UUID

--- app/Lib/Helper.php
+++ app/Lib/Helper.php
@@ -20,3 +20,28 @@ if(!function_exists('show_class')) {
     return call_user_func_array('Request::is', (array)$path) ? 'show' : '';
   }
 }
+
+// For generateUUID
+if(!function_exists('generateUUID')){
+	function generateUUID() {
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+			// 32 bits for "time_low"
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_mid"
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_hi_and_version",
+			// four most significant bits holds version number 4
+			mt_rand( 0, 0x0fff ) | 0x4000,
+
+			// 16 bits, 8 bits for "clk_seq_hi_res",
+			// 8 bits for "clk_seq_low",
+			// two most significant bits holds zero and one for variant DCE1.1
+			mt_rand( 0, 0x3fff ) | 0x8000,
+
+			// 48 bits for "node"
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+		);
+	}
+}
