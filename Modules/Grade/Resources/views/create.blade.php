b8e53ff69a1f2213ed6810528a1ab2e4cc70d7ec
mauldoto <maulanaly97@gmail.com>
2022-01-28 00:06:44 +0700

    fixing page edit and create grade

--- Modules/Grade/Resources/views/create.blade.php
+++ Modules/Grade/Resources/views/create.blade.php
@@ -4,19 +4,15 @@
 <div class="col-md-6 grid-margin stretch-card">
   <div class="card">
     <div class="card-body">
-      <h4 class="card-title">Tambah Data Mata Pelajaran</h4>
+      <h4 class="card-title">Tambah Data Kelas</h4>
       <p class="card-description">
-        Form untuk menambah data mata pelajaran
+        Form untuk menambah data kelas
       </p>
-      <form action="{{ url('mapel/store') }}" method="POST">
+      <form action="{{ url('grade/store') }}" method="POST">
         @csrf
         <div class="form-group">
-          <label for="subjectCode">Kode Mata Pelajaran</label>
-          <input type="text" class="form-control" id="subjectCode" name="subject_code" placeholder="Kode">
-        </div>
-        <div class="form-group">
-          <label for="subjectName">Nama Mata Pelajaran</label>
-          <input type="text" class="form-control" id="subjectName" name="subject_name" placeholder="Nama">
+          <label for="gradeName">Nama Kelas</label>
+          <input type="text" class="form-control" id="gradeName" name="grade_name" placeholder="Nama Kelas">
         </div>
         <button type="submit" class="btn btn-primary me-2">Submit</button>
       </form>
--- Modules/Grade/Resources/views/edit.blade.php
+++ Modules/Grade/Resources/views/edit.blade.php
@@ -4,19 +4,15 @@
 <div class="col-md-6 grid-margin stretch-card">
   <div class="card">
     <div class="card-body">
-      <h4 class="card-title">Edit Data Mata Pelajaran</h4>
+      <h4 class="card-title">Tambah Data Kelas</h4>
       <p class="card-description">
-        Form untuk mengubah data mata pelajaran
+        Form untuk mengubah data kelas
       </p>
-      <form action="{{ url('mapel/update', $subject->id) }}" method="POST">
+      <form action="{{ url('grade/update') }}" method="POST">
         @csrf
         <div class="form-group">
-          <label for="subjectCode">Kode Mata Pelajaran</label>
-          <input type="text" class="form-control" id="subjectCode" name="subject_code" placeholder="Kode" value="{{ $subject->subject_code }}">
-        </div>
-        <div class="form-group">
-          <label for="subjectName">Nama Mata Pelajaran</label>
-          <input type="text" class="form-control" id="subjectName" name="subject_name" placeholder="Nama" value="{{ $subject->subject_name }}">
+          <label for="gradeName">Nama Kelas</label>
+          <input type="text" class="form-control" id="gradeName" name="grade_name" placeholder="Nama Kelas" value="{{ $grade->name }}">
         </div>
         <button type="submit" class="btn btn-primary me-2">Submit</button>
       </form>
